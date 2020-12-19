f57ae14e20ceaeac1f3a36ad6c8cd158afcbe3f4
dperk6 <daniel@perklin.ca>
2020-12-18 23:35:12 -0500

    fix

--- api/CheckFollowers/index.js
+++ api/CheckFollowers/index.js
@@ -1,14 +1,14 @@
 const Twitter = require("twitter");
 
 const client = new Twitter({
-  consumer_key: 'YYWi0Kgvu7jx4PRUNhDNipr3w',
-  consumer_secret: 'AFmyVRXWuXDfs0AzkdJTQk5pUnsMVss3GDeYrpLcXI7FnKpSJg',
-  access_token_key: '3120501443-ZqqoLPxUcPIDsdMcPja7m7z935Z5EWRxVOLabMn',
-  access_token_secret: 'GGXrhmqqHLo0poBJ5czxHzHMmHmv6jU7ihD1FHEGyGTL8'
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_SECRET
 });
 
 module.exports = async function (context, req) {
-  let params = { screen_name: "QFinanceDeFi" }
+  let params = { screen_name: process.env.TWITTER_USERNAME }
   let data = await client.get('followers/list', params).then(res => {
     return res.users
   })
